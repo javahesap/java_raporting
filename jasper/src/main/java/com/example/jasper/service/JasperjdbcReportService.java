02ca3b38aa982647b289c548435729abe7249799
Beşir AYDEMİR <besir.aydemir@hmb.gov.tr>
2025-01-20 12:47:36 +0300

    jasper içinde sql örenek

--- jasper/src/main/java/com/example/jasper/service/JasperjdbcReportService.java
+++ jasper/src/main/java/com/example/jasper/service/JasperjdbcReportService.java
@@ -1,4 +1,5 @@
 package com.example.jasper.service;
+
 import net.sf.jasperreports.engine.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,47 +10,52 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class JasperjdbcReportService {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+	public Connection getConnect() throws SQLException {
+		String url = "jdbc:mariadb://127.0.0.1:3306/contact";
+		String username = "root";
+		String password = "";
+		Connection connection = DriverManager.getConnection(url, username, password);
+		return connection;
 
-    public byte[] createPersonelReport() {
-        try {
-            // JDBC bağlantısı kurma
-            String url = "jdbc:mariadb://127.0.0.1:3306/contact";
-            String username = "root";
-            String password = "";
-            
-            Connection connection = DriverManager.getConnection(url, username, password);
+	}
 
-            // SQL sorgusunu çalıştırma
-            String query = "SELECT * FROM personel";
-            ResultSet resultSet = connection.createStatement().executeQuery(query);
+	public byte[] createPersonelReport() {
 
-            // JasperReports için veri kaynağı oluşturma
-            JRResultSetDataSource dataSource = new JRResultSetDataSource(resultSet);
+		try {
+			// JDBC bağlantısı kurma
+			// SQL sorgusunu çalıştırma
+			String query = "SELECT * FROM personel";
+			ResultSet resultSet = getConnect().createStatement().executeQuery(query);
 
-            // Şablon dosyasını yükle
-            InputStream reportStream = getClass().getResourceAsStream("/reports/personel_listesijdbc.jrxml");
-            JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
+			// JasperReports için veri kaynağı oluşturma
+			JRResultSetDataSource dataSource = new JRResultSetDataSource(resultSet);
 
-            // Parametreler (eğer varsa)
-            Map<String, Object> parameters = new HashMap<>();
-            parameters.put("REPORT_TITLE", "Personel Raporu");
+			// Şablon dosyasını yükle
+			InputStream reportStream = getClass().getResourceAsStream("/reports/personel_listesijdbc.jrxml");
+			JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
 
-            // Raporu doldur
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+			// Parametreler (eğer varsa)
+			Map<String, Object> parameters = new HashMap<>();
+			parameters.put("REPORT_TITLE", "Personel Raporu");
 
-            // PDF olarak dışa aktar
-            return JasperExportManager.exportReportToPdf(jasperPrint);
+			// Raporu doldur
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
 
-        } catch (Exception e) {
-            throw new RuntimeException("Rapor oluşturulurken bir hata oluştu.", e);
-        }
-    }
+			// PDF olarak dışa aktar
+			return JasperExportManager.exportReportToPdf(jasperPrint);
+
+		} catch (Exception e) {
+			throw new RuntimeException("Rapor oluşturulurken bir hata oluştu.", e);
+		}
+	}
 }
