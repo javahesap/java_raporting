954ed118b361a80e6d052dd8a8a78e48896469a6
Beşir AYDEMİR <besir.aydemir@hmb.gov.tr>
2025-01-20 16:39:24 +0300

    düzeltme yapıldı

---
+++ jasperserve/src/main/java/jasperserve/example/JasperserveApplication.java
@@ -0,0 +1,13 @@
+package jasperserve.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperserveApplication.class, args);
+	}
+
+}
